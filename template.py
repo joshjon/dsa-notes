47c7fd4d19d01bdc03a64fa4dc805546bfa8011a
Josh Jon <joshjon@users.noreply.github.com>
2024-07-23 19:34:04 +1000

    Add template.py

---
+++ template.py
@@ -0,0 +1,16 @@
+from util import assert_answer
+
+
+def problem(param: str) -> bool:
+    """
+    Description...
+    """
+
+    return False
+
+
+
+if __name__ == '__main__':
+    data = "leetcode"
+    want = False
+    assert_answer(want, problem(data))
