157f12008b2fa9bae243d4e45a456cb0123703a0
joshjon <joshjon.dev@gmail.com>
2024-07-23 16:06:03 +1000

    Add array and strings challenge 5 & 6

---
+++ arrays-and-strings/challenges/5_running_sum_of_1d_array.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from util import assert_answer
+
+
+class Solution:
+    @staticmethod
+    def running_sum(nums: List[int]) -> List[int]:
+        """
+        Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+        Return the running sum of nums.
+
+        Input: nums = [1,2,3,4]
+        Output: [1,3,6,10]
+        """
+        for i in range(1, len(nums)):
+            nums[i] = nums[i] + nums[i - 1]
+        return nums
+
+
+if __name__ == '__main__':
+    data = [3, 1, 2, 10, 1]
+    want = [3, 4, 6, 16, 17]
+    got = Solution.running_sum(data)
+    assert_answer(want, got, data)
